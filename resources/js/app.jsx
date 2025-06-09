1e92609e6d26f398a36709171b74b2e69650a13f
junaeidysetiawan <junaeidys0@gmail.com>
2025-06-10 00:36:51 +0700

    setup project

---
+++ resources/js/app.jsx
@@ -0,0 +1,33 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+import {HeroUIProvider} from "@heroui/react";
+import { Toaster } from "react-hot-toast";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(
+            <HeroUIProvider>
+                <Toaster position="top-right" />
+                <App {...props} />
+            </HeroUIProvider>
+        );
+    },
+    progress: {
+        color: "#0394fc",
+        showSpinner: true,
+    },
+});
